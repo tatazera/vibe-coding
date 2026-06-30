7d843986b57b53c4f2efc21ba45877bd8625e0a4
tatazera <jtnvieira@gmail.com>
2026-06-30 13:38:59 -0300

    EVA Stand1 v1.2.0 - primeiro push (plugin completo + auto-update + versoes_anteriores)

---
+++ STAND1_EVA/STAND1_EVA_loader.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+require 'sketchup.rb'
+require 'extensions.rb'
+
+eva_ext = SketchupExtension.new('EVA Stand1', 'STAND1_EVA/core')
+eva_ext.description = 'Exportador padronizado de Scenes e gerador de prompts para render com IA (Nano Banana 2).'
+eva_ext.version     = '1.2.0'
+eva_ext.creator     = 'Stand1 Producoes'
+eva_ext.copyright   = '2025 Stand1 Producoes'
+
+Sketchup.register_extension(eva_ext, true)
