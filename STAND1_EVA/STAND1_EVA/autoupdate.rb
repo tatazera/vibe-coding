2fadecabffa09ce459c9bdb37b649de2916fe0dd
tatazera <jtnvieira@gmail.com>
2026-07-06 20:37:49 -0300

    auto: publish EVA v1.4.4 (STAND1_EVA/STAND1_EVA/autoupdate.rb)

--- STAND1_EVA/STAND1_EVA/autoupdate.rb
+++ STAND1_EVA/STAND1_EVA/autoupdate.rb
@@ -9,7 +9,7 @@ module STAND1
 
     module AutoUpdate
 
-      VERSION       = '1.4.3'
+      VERSION       = '1.4.4'
       URL_MANIFESTO = 'https://raw.githubusercontent.com/tatazera/vibe-coding/main/STAND1_EVA/latest.json'
 
       # Compara "a.b.c" numericamente: remota > local?
